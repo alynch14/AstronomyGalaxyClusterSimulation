ad1d72679818cb21200d1974ef83bdad2d8c1819
Adam Lynch <alynch14@students.towson.edu>
2019-05-08 18:55:23 -0400

    Added Camera Script, finalized PlaneScript. Camera moves to the correct position, but its rotation needs to be corrected.

--- Astronomy Simulation/Assets/Scripts/CameraScript.cs
+++ Astronomy Simulation/Assets/Scripts/CameraScript.cs
@@ -6,19 +6,44 @@ public class CameraScript : MonoBehaviour
 {
     public GameObject finalPicture;
     private static Vector3 POSITION_MOVING_TOWARDS = new Vector3(31.7f, 5.7f, -8f);
+    private int angle;
+    private Transform startPosition;
     // Start is called before the first frame update
     void Start()
     {
-        
+        startPosition = transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!finalPicture.activeSelf && !transform.position.Equals(POSITION_MOVING_TOWARDS))
+        if (!finalPicture.activeSelf)
         {
-            transform.Translate(new Vector3(0.1f, 0.1f));
-            transform.Rotate(new Vector3(-0.5f, -0.5f, 0));
+            //   transform.Translate(new Vector3(0.1f, 0.1f));
+           /* if(Mathf.Abs(transform.position.y - POSITION_MOVING_TOWARDS.y) < 0.5){
+                
+            }
+            else if (Mathf.Abs(transform.position.x - POSITION_MOVING_TOWARDS.x) < 0.5 )
+            {
+                transform.Translate(new Vector3(0f, -0.1f, 0f));
+                transform.localRotation.Set(transform.rotation.x, transform.rotation.y, 90.0f, transform.rotation.w);
+                
+            }
+            else
+            {*/
+                transform.position = Vector3.Lerp(startPosition.position, POSITION_MOVING_TOWARDS, Time.deltaTime / 2);
+            StartCoroutine("RotateCamera");
+            
+            //}
+        }
+    }
+
+    IEnumerator RotateCamera()
+    {
+        while(!(Mathf.Abs(transform.rotation.y +90) < 0.0005))
+        {
+            //transform.Rotate(new Vector3(-0.5f, -0.5f, 0.0f));
+            yield return new WaitForSeconds(0.1f);
         }
     }
 }
---
+++ Astronomy Simulation/Assets/Scripts/PlaneScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneScript : MonoBehaviour
+{
+    public GameObject camera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(camera.transform.position.y-transform.position.y > 40.0f)
+        {
+            transform.Translate(0.0f, 0.01f, 0.0f, Space.World);
+        }
+        else
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
