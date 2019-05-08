66225c0f188691861137a773571279fcba3542d2
Adam Lynch <alynch14@students.towson.edu>
2019-05-07 23:06:09 -0400

    Set up title screen and got the script working for the two stars that orbit the black hole at the center of the universe. Added final Pictures to be used in fly-through to the prefabs. Wrote a script for the camera that needs to be tested to see if it does what it is supposed to do.

---
+++ Astronomy Simulation/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public GameObject finalPicture;
+    private static Vector3 POSITION_MOVING_TOWARDS = new Vector3(31.7f, 5.7f, -8f);
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!finalPicture.activeSelf && !transform.position.Equals(POSITION_MOVING_TOWARDS))
+        {
+            transform.Translate(new Vector3(0.1f, 0.1f));
+            transform.Rotate(new Vector3(-0.5f, -0.5f, 0));
+        }
+    }
+}
---
+++ Astronomy Simulation/Assets/Scripts/S2Orbit.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class S2Orbit : MonoBehaviour
+{
+    public GameObject blackHole;
+    private Transform blackHolePosition;
+    private float angle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        blackHolePosition = blackHole.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        angle = Mathf.Abs(transform.localPosition.y/2.0f);
+        gameObject.transform.RotateAround(new Vector3(blackHolePosition.position.x, (float)(blackHolePosition.position.y+2.0), blackHolePosition.position.z), blackHolePosition.position, angle);
+    }
+}
